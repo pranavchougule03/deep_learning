214359bf5422453524aeb9f397417756980667e1
pranavchougule03 <124160569+pranavchougule03@users.noreply.github.com>
2024-01-07 14:09:41 +0530

    port change

--- app.py
+++ app.py
@@ -22,5 +22,5 @@ def extract_text():
     return jsonify({'text': extracted_text}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
 
