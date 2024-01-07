50e4ba07e50dae4aabafd4b2bb320aae97df7ed2
pranavchougule03 <124160569+pranavchougule03@users.noreply.github.com>
2024-01-07 23:55:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,30 @@ import decode
 import numpy as np
 from PIL import Image
 import io
+import base64
 
 model = load_model('first_prediction_model.keras')
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
- return 'Hello World!'
-
 @app.route('/ocr', methods=['POST'])
 def extract_text():
-    image_data = request.files['image'].read()
+    data = request.get_json()  # Receive JSON object
 
-    image = Image.open(io.BytesIO(image_data))
-    image = np.array(image)
+    # Extract Base64-encoded image data
+    base64_image_data = data.get('image')
+
+    # Decode Base64 data
+    decoded_image_data = base64.b64decode(base64_image_data)
+
+    # Convert decoded data into a PIL image
+    image = Image.open(io.BytesIO(decoded_image_data))
+    # image = np.array(image)
     if(len(image.shape)==2): #if image is grayscale
         image = np.stack((image,)*3, axis=-1)
+        return jsonify({'text': 'grayscale image'}), 200
+        # print(image.shape)
+    # print(image.shape)
     extracted_text = decode.extract_text(image,model)
     print(extracted_text)
     return jsonify({'text': extracted_text}), 200
