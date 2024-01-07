92508b19fc311757f6a38b5f33854cfe15f6e6af
pranavchougule03 <124160569+pranavchougule03@users.noreply.github.com>
2024-01-07 11:07:48 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-image_path = 'data/2b827.png'
-api_url = 'http://localhost:5000/ocr'
-
-files = {'image': open(image_path, 'rb')}  # Open image in binary mode
-response = requests.post(api_url, files=files)  # Use 'files' parameter
-
-# Handle response as before
